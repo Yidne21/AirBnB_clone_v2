0e8af863dacd8ea0020602015b9f8ddbbdec338f
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-10-16 11:13:51 +0300

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/pyhton3
 """this flask script starts a flask web app"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_flask():
     """Return string when route is fired"""
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
